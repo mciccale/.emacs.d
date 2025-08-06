5a9a740ae186e029c26d7bbbe24563743bdabdb8
Marco Ciccalè Baztán <marcociccalebaztan@gmail.com>
2025-08-06 11:13:47 +0200

    lsp-mode

--- elisp/languages-config.el
+++ elisp/languages-config.el
@@ -10,11 +10,67 @@
   :after emacs
   :custom
   (treesit-auto-install 'prompt)
+  :init
+  (setq treesit-language-source-alist '((json "https://github.com/tree-sitter/tree-sitter-json")))
   :config
+  (setq treesit-auto-langs '(json toml yaml))
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode t))
 
-;; Eglot
+;; LSP
+(use-package lsp-mode
+  :ensure t
+  :straight t
+  :defer t
+  :hook ((lsp-mode . lsp-enable-which-key-integration)
+         ((lean4-mode) . lsp-deferred))
+  :commands lsp
+  :custom
+  (lsp-keymap-prefix "C-c l")
+  (lsp-inlay-hint-enable t)
+  (lsp-completion-provider :none)
+  (lsp-session-file (locate-user-emacs-file ".lsp-session"))
+  (lsp-log-io nil)
+  (lsp-idle-delay 0.5)
+  (lsp-keep-workspace-alive nil)
+  ;; Core settings
+  (lsp-enable-xref t)
+  (lsp-auto-configure t)                                ;; Automatically configure LSP.
+  (lsp-enable-links nil)                                ;; Disable links.
+  (lsp-eldoc-enable-hover t)                            ;; Enable ElDoc hover.
+  (lsp-enable-file-watchers nil)                        ;; Disable file watchers.
+  (lsp-enable-folding nil)                              ;; Disable folding.
+  (lsp-enable-imenu t)                                  ;; Enable Imenu support.
+  (lsp-enable-indentation nil)                          ;; Disable indentation.
+  (lsp-enable-on-type-formatting nil)                   ;; Disable on-type formatting.
+  (lsp-enable-suggest-server-download t)                ;; Enable server download suggestion.
+  (lsp-enable-symbol-highlighting t)                    ;; Enable symbol highlighting.
+  (lsp-enable-text-document-color t)                    ;; Enable text document color.
+  ;; Modeline settings
+  (lsp-modeline-code-actions-enable nil)                ;; Keep modeline clean.
+  (lsp-modeline-diagnostics-enable nil)                 ;; Use `flymake' instead.
+  (lsp-modeline-workspace-status-enable t)              ;; Display "LSP" in the modeline when enabled.
+  (lsp-signature-doc-lines 1)                           ;; Limit echo area to one line.
+  (lsp-eldoc-render-all t)                              ;; Render all ElDoc messages.
+  ;; Completion settings
+  (lsp-completion-enable t)                             ;; Enable completion.
+  (lsp-completion-enable-additional-text-edit t)        ;; Enable additional text edits for completions.
+  (lsp-enable-snippet nil)                              ;; Disable snippets
+  (lsp-completion-show-kind t)                          ;; Show kind in completions.
+  ;; Lens settings
+  (lsp-lens-enable t)                                   ;; Enable lens support.
+  ;; Headerline settings
+  (lsp-headerline-breadcrumb-enable nil)   ;; Enable symbol numbers in the headerline.
+  ;; Semantic settings
+  (lsp-semantic-tokens-enable t))                     ;; Disable semantic tokens.
+
+;; Lean4
+(use-package lean4-mode
+  :ensure t
+  :straight (lean4-mode :type git :host github
+                        :repo "leanprover-community/lean4-mode"
+                        :files ("*.el" "data"))
+  :commands lean4-mode)
 
 ;; Auto-match parentheses
 (use-package smartparens
